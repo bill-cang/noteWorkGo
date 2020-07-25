7727043aa2b3d940d8708e409f22328408d78f75
ckx0709 <178709230@qq.com>
2020-07-25 17:51:44 +0800

    commit amend test

--- bestLib/utils/sensitiveWords_test.go
+++ bestLib/utils/sensitiveWords_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestAddSensitiveToMap(t *testing.T) {
-	text := "文明用语你&* 妈, 逼的你这个狗 日的，怎么这么傻啊。我也是服了，我日,这些话我都说不出口"
+	text := "文明用语你&* 妈, 逼的你这个狗 日的，怎么这么傻啊。我也是服了，我日,这些话我都说不出口bo1"
 	fmt.Println(ChangeSensitiveWords(text, sensitiveWord))
 }
 
