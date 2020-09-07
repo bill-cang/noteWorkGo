4bcb05c1f191fbe3c13c22325a9a08f855d9b13b
ckx0709 <178709230@qq.com>
2020-09-07 10:29:25 +0800

    工具-小数计算

--- keyword/number/intConvertFloat/intAndFloat.go
+++ keyword/number/intConvertFloat/intAndFloat.go
@@ -1,17 +1,47 @@
-package infl
+package main
 
 import (
 	"fmt"
 	"github.com/shopspring/decimal"
 )
 
-func CalcMul() {
-	decimal.DivisionPrecision = 2 // 保留两位小数，如有更多位，则进行四舍五入保留两位小数
+func main() {
+	price, err := decimal.NewFromString("1.2345678")
+	if err != nil {
+		panic(err)
+	}
+	//保留小数位，四舍五入
+	rd := price.Round(3)
+	//保留小数位，向上取
+	cash := price.RoundCash(5)
 
-	var num1 float64 = 3.36
-	var num2 int = 2
+	f, of := rd.Float64()
+	if of {
+		return
+	}
+	fmt.Printf("mul :=%f ,cash =%+v", f, cash)
+}
 
-	test := decimal.NewFromFloat(num1).Mul(decimal.NewFromFloat(float64(num2))).Round(0)
-	f, _ := test.Float64()
-	fmt.Println(int(f))
+func main1() {
+	price, err := decimal.NewFromString("136.02")
+	if err != nil {
+		panic(err)
+	}
+
+	quantity := decimal.NewFromInt(3)
+
+	fee, _ := decimal.NewFromString(".035")
+	taxRate, _ := decimal.NewFromString(".08875")
+
+	subtotal := price.Mul(quantity)
+
+	preTax := subtotal.Mul(fee.Add(decimal.NewFromFloat(1)))
+
+	total := preTax.Mul(taxRate.Add(decimal.NewFromFloat(1)))
+
+	fmt.Println("Subtotal:", subtotal)                      // Subtotal: 408.06
+	fmt.Println("Pre-tax:", preTax)                         // Pre-tax: 422.3421
+	fmt.Println("Taxes:", total.Sub(preTax))                // Taxes: 37.482861375
+	fmt.Println("Total:", total)                            // Total: 459.824961375
+	fmt.Println("Tax rate:", total.Sub(preTax).Div(preTax)) // Tax rate: 0.08875
 }
